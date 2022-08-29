11bfb18525143f36a83816efafe1c0fdea8ad4b4
Peter Ly <peter.ly1@hotmail.com>
2022-08-29 14:23:27 +0100

    Connected frontend to backend

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -5,13 +5,24 @@ import { images } from '../../constants'
 
 import './About.scss';
 
-const abouts = [
-  { title: 'Frontend Development', description: 'I am good at frontend developer.', imgUrl: images.about01 },
-  { title: 'Backend Development', description: 'I am a good backend developer.', imgUrl: images.about02 },
-  { title: 'Web Design', description: 'I am a good web designer.', imgUrl: images.about03 }
-]
+import { urlFor, client } from '../../client';
+
 
 const About = () => {
+
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+  const query = '*[_type == "abouts"]';
+
+  client.fetch(query)
+    .then((data) => {
+      setAbouts(data)
+    })
+  }, []);
+  
+
+  
   return (
     <>
       <h2 className="head-text">
@@ -26,7 +37,7 @@ const About = () => {
           className="app__profile-item"
           key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
             <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
 
