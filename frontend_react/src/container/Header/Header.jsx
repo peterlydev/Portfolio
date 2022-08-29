9e1ce926a3b9478742f200568225931816a8b529
Peter Ly <peter.ly1@hotmail.com>
2022-08-29 12:36:58 +0100

    Added about section

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -15,7 +15,8 @@ const scaleVariants = {
   },
 };
 
-const Header = () => (
+const Header = () => {
+  return (
     <div id="home" className="app__header app__flex">
       <motion.div
         whileInView={{ x: [-100, 0], opacity: [0, 1] }}
@@ -58,7 +59,7 @@ const Header = () => (
         whileInView={scaleVariants.whileInView}
         className="app__header-circles"
       >
-        {[images.javascript, images.node, images.react].map((circle, index) => (
+        {[images.javascript, images.react, images.node].map((circle, index) => (
           <div className="circle-cmp app__flex" key={`circle-${index}`}>
             <img src={circle} alt="circle" />
           </div>
@@ -66,5 +67,6 @@ const Header = () => (
       </motion.div>
     </div>
   );
+};
 
 export default Header;
